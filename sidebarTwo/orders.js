941113937752a6cb826544e0260a5c5166686d4d
mayur <mayur.gavande308@gmail.com>
2023-11-03 14:54:40 +0530

    sidebar initial commit

---
+++ sidebarTwo/orders.js
@@ -0,0 +1,37 @@
+const Orders = [
+    {
+        productName: 'Folder Mini drone',
+        productNumber: '85643',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+
+    },
+    {
+        productName: 'Folder Mini drone',
+        productNumber: '85643',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+
+    },
+    {
+        productName: 'Folder Mini drone',
+        productNumber: '85643',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+
+    },
+    {
+        productName: 'Folder Mini drone',
+        productNumber: '85643',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+
+    },
+    {
+        productName: 'Folder Mini drone',
+        productNumber: '85643',
+        paymentStatus: 'Due',
+        shipping: 'pending'
+
+    }
+]
